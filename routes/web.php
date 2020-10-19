eae0bccfec55d98e6d3f8bbe7b03d50527416e70
Mohammad Gharbat <arbioun@gmail.com>
2020-10-19 19:21:54 +0300

    update reoute

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tweets.index');
 });
 
 Route::get("/login", [\App\Http\Controllers\AuthController::class, "login"]);
